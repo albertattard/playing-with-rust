c9ac25d8ced1fb51790a9fa80306ca9c5f28430f
Albert Attard <albert.attard@thoughtworks.com>
2021-11-22 08:46:29 +0100

    Add an example

--- operator-overloading/src/lib.rs
+++ operator-overloading/src/lib.rs
@@ -1,1 +1,12 @@
 mod tray;
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Add;
+
+    #[test]
+    fn adds_two_numbers_using_the_add_method() {
+        // We can use the `add()` method instead of the `+`, but we need to import the trait
+        assert_eq!(10_i32.add(5), 15);
+    }
+}
